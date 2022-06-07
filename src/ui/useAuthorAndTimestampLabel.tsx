dd13fa13456201f31112a0316b1f7f95bc5621d5
Stef Busking <stef.busking@gmail.com>
2022-06-07 11:33:34 +0200

    Fix TSDoc

--- src/ui/useAuthorAndTimestampLabel.tsx
+++ src/ui/useAuthorAndTimestampLabel.tsx
@@ -11,17 +11,19 @@ import t from 'fontoxml-localization/src/t';
 const configuredScope = configurationManager.get('scope');
 
 /**
- * A custom React hook that formats the author and timestamp of the given reviewAnnotation or reply.
+ * A custom React hook that formats the author and timestamp of the given
+ * reviewAnnotation or reply.
  *
- * Renders the author name as "You" if the author id matches the current scope user id.
+ * Renders the author name as "You" if the author id matches the current scope
+ * user id.
  *
  * The timestamp label will not contain any kind of dashes.
  *
- * @param reviewAnnotationOrReply               -
- * @param [isReviewAnnotationResolved=false]    - If set to true it uses the resolvedAuthor and
- * resolvedTimestamp fields of the given reviewAnnotation, otherwise it uses the (created) author
- * and timestamp fields.
- * @param  [fallback=t('Author not available')] - A string to display when the (resolved)
+ * @param reviewAnnotationOrReply    -
+ * @param isReviewAnnotationResolved - If set to true it uses the resolvedAuthor
+ * and resolvedTimestamp fields of the given reviewAnnotation, otherwise it uses
+ * the (created) author and timestamp fields.
+ * @param fallback                   - A string to display when the (resolved)
  * author field is not set on the given reviewAnnotationOrReply.
  *
  * @returns Object containing the 'author' and the 'timestamp' keys.
