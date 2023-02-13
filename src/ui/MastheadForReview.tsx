ac816759e99594c593a3b25fc175f7e44e7edd8f
m-verdonk <marloes.verdonk@liones.nl>
2023-02-13 12:18:53 +0100

    DEV-17019 Add buttons for wide canvas and zoom to review masthead

--- src/ui/MastheadForReview.tsx
+++ src/ui/MastheadForReview.tsx
@@ -2,14 +2,19 @@ import * as React from 'react';
 
 import configurationManager from 'fontoxml-configuration/src/configurationManager';
 import {
+	ButtonWithDrop,
+	Drop,
 	Flex,
 	Icon,
 	Label,
 	Masthead,
 	MastheadAlignRight,
 	MastheadContent,
+	Menu,
 } from 'fontoxml-design-system/src/components';
 import ReviewLogo from 'fontoxml-feedback/src/ReviewLogo';
+import FxOperationButton from 'fontoxml-fx/src/FxOperationButton';
+import FxOperationMenuItem from 'fontoxml-fx/src/FxOperationMenuItem';
 
 const configuredScope = configurationManager.get('scope') as {
 	user: {
@@ -31,6 +36,22 @@ const MastheadForReview = (): JSX.Element => {
 							<Icon icon="user" />
 
 							<Label>{displayName}</Label>
+							<FxOperationButton operationName=":toggle-wide-canvas" />
+							<ButtonWithDrop
+								icon="search-plus"
+								label="Zoom"
+								renderDrop={() => (
+									<Drop>
+										<Menu>
+											<FxOperationMenuItem operationName="zoom-content-view-to-75%-75%" />
+											<FxOperationMenuItem operationName="zoom-content-view-to-100%-100%" />
+											<FxOperationMenuItem operationName="zoom-content-view-to-125%-125%" />
+											<FxOperationMenuItem operationName="zoom-content-view-to-150%-150%" />
+											<FxOperationMenuItem operationName="zoom-content-view-to-200%-200%" />
+										</Menu>
+									</Drop>
+								)}
+							/>
 						</Flex>
 					</MastheadAlignRight>
 				)}
