f1acb8ecfb2cee32ecfecb7b96b6a7633845e194
Rober <r.e.bautista.prieto@hotmail.com>
2023-07-14 16:01:33 +0200

    DEV-16363 Add FxProfileChip to draft forms

--- src/ui/AuthorAndTimestampLabel.tsx
+++ src/ui/AuthorAndTimestampLabel.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Flex, Label } from 'fontoxml-design-system/src/components';
+import { Flex } from 'fontoxml-design-system/src/components';
 import type {
 	ReviewAnnotation,
 	ReviewCardContentComponentProps,
@@ -22,55 +22,21 @@ const AuthorAndTimestampLabel: React.FC<Props> = ({
 	reviewAnnotation,
 	isReviewAnnotationResolved = false,
 }) => {
-	const { author: authorData, timestamp: timestampLabel } =
+	const { authorId, timestamp: timestampLabel } =
 		useAuthorAndTimestampLabel(
 			reviewAnnotation,
 			isReviewAnnotationResolved
 		);
 
-	// Make sure the author label is not truncated too much and not too little,
-	// making it and the timestamp label visible.
-	const authorLabelRef = React.useRef<HTMLElement>(null);
-
-	const handleAuthorLabelRef = (domNode: HTMLElement) => {
-		authorLabelRef.current = domNode;
-	};
-
-	const [authorLabelScrollWidth, setAuthorLabelScrollWidth] =
-		React.useState<number>(0);
-
-	React.useEffect(() => {
-		if (authorLabelRef.current) {
-			setAuthorLabelScrollWidth(authorLabelRef.current.scrollWidth);
-		}
-	}, []);
-
 	return (
 		<Flex alignItems="center" spaceSize="s">
-			{authorData.id && (
-				<FxProfileChip
-					profileId={authorData.id}
-					secondaryLabel={{
-						value: timestampLabel,
-						position: 'below',
-					}}
-				/>
-			)}
-
-			{authorData.displayName && (
-				<Flex
-					flex="0 1 auto"
-					style={{ minWidth: Math.min(authorLabelScrollWidth, 32) }}
-				>
-					<Label
-						tooltipContent={authorData.displayName}
-						data-test-id="author-label"
-						onRef={handleAuthorLabelRef}
-					>
-						{authorData.displayName}
-					</Label>
-				</Flex>
-			)}
+			<FxProfileChip
+				profileId={authorId}
+				secondaryLabel={timestampLabel ? {
+					value: timestampLabel,
+					position: 'below',
+				} : null}
+			/>
 		</Flex>
 	);
 };
