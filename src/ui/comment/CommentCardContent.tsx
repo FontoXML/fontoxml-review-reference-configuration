535466eaee175dafe024e70625d110b29367b2fc
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2024-09-05 13:49:34 +0200

    DEV-14976 Add/use useReviewAnnotationRangeVisibility in cards. Tests are green. TODO: do not merge on develop ;)

--- src/ui/comment/CommentCardContent.tsx
+++ src/ui/comment/CommentCardContent.tsx
@@ -11,6 +11,7 @@ import ReviewAnnotationStatus from 'fontoxml-feedback/src/ReviewAnnotationStatus
 import ReviewBusyState from 'fontoxml-feedback/src/ReviewBusyState';
 import ReviewRecoveryOption from 'fontoxml-feedback/src/ReviewRecoveryOption';
 import type { ReviewCardContentComponentProps } from 'fontoxml-feedback/src/types';
+import useReviewAnnotationRangeVisibility from 'fontoxml-feedback/src/useReviewAnnotationRangeVisibility';
 import t from 'fontoxml-localization/src/t';
 
 import commentTypes from '../commentTypes';
@@ -99,6 +100,12 @@ function CommentCardContent({
 		}
 	}, [focusableRef, showFooter]);
 
+	// This is only here as an example
+	const reviewRangeVisibility = useReviewAnnotationRangeVisibility(
+		reviewAnnotation.id,
+		context
+	);
+
 	if (
 		reviewAnnotation.isLoading &&
 		reviewAnnotation.busyState === ReviewBusyState.REFRESHING
@@ -156,6 +163,7 @@ function CommentCardContent({
 		<Block
 			paddingSize="m"
 			dataTestId="fontoxml-review-reference-configuration-comment-card-content"
+			data-review-annotation-range-visibility={reviewRangeVisibility.toLowerCase()}
 			data-review-annotation-state={reviewAnnotation.busyState}
 			data-review-annotation-type={reviewAnnotation.type}
 			data-review-annotation-comment-type={
