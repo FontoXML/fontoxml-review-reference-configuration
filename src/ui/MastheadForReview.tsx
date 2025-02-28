6e0756f2766fd91c55dc2d6e1f244855eecf2477
Alper Tunca <alpertunca@gmail.com>
2025-02-28 14:10:55 +0100

    DEV-23313 Update README link and enhance Review masthead with a zoom dropdown and a wide-canvas button features

--- src/ui/MastheadForReview.tsx
+++ src/ui/MastheadForReview.tsx
@@ -1,23 +1,23 @@
-import * as React from 'react';
+import type { FC } from 'react';
 
 import configurationManager from 'fontoxml-configuration/src/configurationManager';
 import {
+	ButtonWithDrop,
+	Drop,
 	Flex,
 	Masthead,
 	MastheadAlignRight,
 	MastheadContent,
+	Menu,
 } from 'fontoxml-design-system/src/components';
 import ReviewLogo from 'fontoxml-feedback/src/ReviewLogo';
+import FxOperationButton from 'fontoxml-fx/src/FxOperationButton';
+import FxOperationMenuItem from 'fontoxml-fx/src/FxOperationMenuItem';
 import FxProfileChip from 'fontoxml-fx/src/FxProfileChip';
 
-const configuredScope = configurationManager.get('scope') as {
-	user?: {
-		displayName?: string,
-		id?: string
-	}
-};
+const configuredScope = configurationManager.get('scope');
 
-const MastheadForReview = (): JSX.Element => {
+const MastheadForReview: FC = () => {
 	const { id } = configuredScope.user ?? {};
 	return (
 		<Masthead>
@@ -26,6 +26,23 @@ const MastheadForReview = (): JSX.Element => {
 
 				<MastheadAlignRight>
 					<Flex spaceSize="m">
+						<FxOperationButton operationName=":toggle-wide-canvas" />
+						<ButtonWithDrop
+							icon="search-plus"
+							label="Zoom"
+							renderDrop={() => (
+								<Drop>
+									<Menu>
+										<FxOperationMenuItem operationName="zoom-content-view-to-75%-75%" />
+										<FxOperationMenuItem operationName="zoom-content-view-to-100%-100%" />
+										<FxOperationMenuItem operationName="zoom-content-view-to-125%-125%" />
+										<FxOperationMenuItem operationName="zoom-content-view-to-150%-150%" />
+										<FxOperationMenuItem operationName="zoom-content-view-to-200%-200%" />
+									</Menu>
+								</Drop>
+							)}
+						/>
+
 						{!!id && <FxProfileChip profileId={id} />}
 					</Flex>
 				</MastheadAlignRight>
